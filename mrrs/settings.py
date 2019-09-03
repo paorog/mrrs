e4060d1b4b4bd031ede695279e2e70625ae665a4
Powa <rogelio_itspecialist@yahoo.com.ph>
2019-09-03 13:50:42 +0800

    install django heroku

--- mrrs/settings.py
+++ mrrs/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 
 import os
 import datetime
+import django_heroku
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -156,3 +157,5 @@ EMAIL_HOST_USER = 'arogelio13@gmail.com'
 EMAIL_HOST_PASSWORD = 'K!77u@07'
 EMAIL_USE_TLS = True
 EMAIL_USE_SSL = False
+
+django_heroku.settings(locals())
