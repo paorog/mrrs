a7a2aacb4a11a4965b9a7f9eb1f4bb788f39c09e
Powa <rogelio_itspecialist@yahoo.com.ph>
2019-10-14 10:16:36 +0800

    change db

--- mrrs/settings.py
+++ mrrs/settings.py
@@ -116,7 +116,7 @@ DATABASES = {
     # }
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, 'mrrs-live.sqlite3'),
     }
 
 }
